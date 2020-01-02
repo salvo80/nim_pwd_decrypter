31a25486590b8e5a5a69e5933df334aa3dbbd267
lupo <salvatore.lupo@eng.it>
2020-01-02 17:41:10 +0100

    fix

--- multi_thread.nim
+++ multi_thread.nim
@@ -1,38 +1,37 @@
 import nimSHA2
 import times
-import os,threadpool
+import os,threadpool, strutils
 
 
 var chan: Channel[string]
+var esito: Channel[string]
 let time = cpuTime()
 
 proc worker(test:string)=
   var line = chan.recv()
-  while computeSHA256(line).hex!=test:
-    #echo getThreadId(),line
+  while line != "*_fine_*" and computeSHA256(line).hex != test:
+    #echo getThreadId(),line," ",computeSHA256(line).hex
     line = chan.recv()
-    
-  #if computeSHA256(line).hex==test:
-  echo "trovato ",line
-  echo "eseguito in ",(cpuTime()-time)
-  chan.close()
-   
-proc find_pwd*(test:string, pwd_file:string)=
+  esito.send(line)
+  echo getThreadId(),"trovato ",line," in ",(cpuTime()-time)
+ 
+proc find_pwd*(otest:string, pwd_file:string)=
+  let test = toUpper(otest)
   chan.open(1000)
-  
+  esito.open()
   spawn worker(test)
   spawn worker(test)
   spawn worker(test)
   spawn worker(test)
-  
   for line in lines pwd_file:
-    if chan.peek()<0:
-      break
     chan.send(line)
+  chan.send("*_fine_*")
+  chan.send("*_fine_*")
+  chan.send("*_fine_*")
+  chan.send("*_fine_*")
+  for i in 1..4:
+    echo esito.recv()
 
-  while chan.peek()>0:
-    sleep(100)
-  
   chan.close()
-
+  echo "end"
 
--- pwd_parallel.nim
+++ pwd_parallel.nim
@@ -2,27 +2,63 @@ import nimSHA2
 import times
 import os
 import lists
+import threadpool, streams, strutils
+{.experimental: "parallel".}
 
-proc calc(arr: DoublyLinkedList, test: string): string = 
+type
+  Res = object
+    word: string
+    hex: string
+    match: bool
+
+proc removeAndReturn(extarr: DoublyLinkedList[string], s: string):string = 
+  #var p:DoublyLinkedNode
+  var arr = extarr
+  var p = arr.find(s)
+  arr.remove(p)
+  return p.value
+
+proc calcAll(extarr: DoublyLinkedList, test: string): string = 
+  var arr = extarr
   for i in arr:
-    if computeSHA256(i).hex==test:
+    if computeSHA256(removeAndReturn(arr, i)).hex == test:
       return test
   return ""
+
+proc calc(s:string, test: string): bool = computeSHA256(s).hex == test
 
 let test = paramStr(1)
 echo test
 var arr = initDoublyLinkedList[string]()
 let time = cpuTime()
+var count = 0
+
+var strm = openFileStream("D:\\salvo\\Downloads\\rockyou.txt")
+let f = strm.readAll().split('\n')
+strm.close()
+var res = newSeq[string]()
+parallel:
+  for line in f:
+    let x = spawn calc(line,test)
+    if x:
+      res.add(line)
+
+for i in res:
+  echo i
+#[
 for line in lines "D:\\salvo\\Downloads\\rockyou.txt":
-  if arr.len > 999:
-    parallel:
-      if spawn calc(arr,test) != "":
-        echo "trovato ",line
+    if count > 999:
+      let word = spawn calc(arr,test)
+      if word != "":
+        echo "trovato ",word
         break
-    arr = initDoublyLinkedList[string]()
-  arr.append(line)
-if arr.len > 0:
-  if spawn calc(arr,test) != "":
-    echo "trovato ",line
+      arr = initDoublyLinkedList[string]()
+    arr.append(line)
+    count = count + 1
+  if arr.head != nil:
+    let word = spawn calc(arr,test)
+    if word != "":
+      echo "trovato ",word
+]#
 echo "eseguito in ",(cpuTime()-time)
 
